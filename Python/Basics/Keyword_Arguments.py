790934c6a9d40a1710b376379435c4184126c1f7
Hyug Her <gurher@ucdavis.edu>
2022-09-19 21:25:42 -0700

    til

---
+++ Python/Basics/Keyword_Arguments.py
@@ -0,0 +1,8 @@
+
+def say_hello(name, age):
+    print(f"Hello {name} you are {age} years old")
+    
+    
+say_hello("Edward", 12) # Positional Arguments
+
+say_hello(age=12,name="Edward") # Keyword Arguments
