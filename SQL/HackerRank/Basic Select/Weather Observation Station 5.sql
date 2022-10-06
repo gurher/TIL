92cc3c0bbc58b6b19a620f2a16f72ea612b9064b
Hyug Her <gurher@ucdavis.edu>
2022-10-06 02:08:47 -0700

    sql_query_update

--- SQL/HackerRank/Basic Select/Weather Observation Station 5.sql
+++ SQL/HackerRank/Basic Select/Weather Observation Station 5.sql
@@ -1,13 +1,23 @@
-SELECT *
-FROM
-  (SELECT CITY, LENGTH(CITY)
-   FROM STATION
-   ORDER BY LENGTH(CITY), CITY)
-WHERE ROWNUM = 1
-UNION
-SELECT *
-FROM
-  (SELECT CITY, LENGTH(CITY)
-   FROM STATION
-   ORDER BY LENGTH(CITY) DESC, CITY)
-WHERE ROWNUM = 1;
+SELECT 
+    final_table.city,
+    final_table.len
+    
+FROM (SELECT
+            city_length.city,
+            city_length.len,
+            ROW_NUMBER() OVER(PARTITION BY city_length.len ORDER BY city_length.city ) AS 'row_number'
+      FROM (   SELECT city, LENGTH(city) AS "len"
+               FROM station
+               WHERE LENGTH(city) = ( SELECT MAX(LENGTH(city))
+                                       FROM station )
+                   OR LENGTH(city) = ( SELECT MIN(LENGTH(city))
+                                       FROM station ) 
+               ORDER BY 2 ASC, 1 ASC ) AS city_length
+      ) AS final_table
+WHERE final_table.row_number = 1   
+
+
+
+
+-- SELECT city, LENGTH(city) FROM station ORDER BY LENGTH(city), city LIMIT 1;
+-- SELECT city, LENGTH(city) FROM station ORDER BY LENGTH(city) DESC, city LIMIT 1;
