c3a9eae2cb90585f638bf784fed0d6786f90771d
Hyug Her <gurher@ucdavis.edu>
2022-11-16 18:59:33 -0800

    push

--- Python/Algorithms/Day1.py
+++ Python/Algorithms/Day1.py
@@ -10,30 +10,42 @@ class Solution:
         return -1
     
     
-    def firstBadVersion(self, n ) :
+    
+    def isBadVersion(self, mid ,t) :
+        self.target = t
+        if mid >= self.target :
+            return True
+        else :
+            return False
+        
+
+    def firstBadVersion(self, n ,t) :
         self.left, self.right = 1, n
         
         while self.left < self.right:
             self.mid = self.left + (self.right - self.left) // 2
-            if Solution.firstBadVersion(self, self.mid):
+            if Solution.isBadVersion(self, self.mid, t):
                 self.right = self.mid
             else:
                 self.left = self.mid + 1
         return self.left
+    
+    
+    def searchInsert(self, nums, target) :        
+        self.nums = nums
+        self.target = target
         
-
-
-
-
-
-
+        if max(self.nums) < self.target :
+            return len(self.nums)
         
-    
-    
-    
+        for idx, num in enumerate(self.nums) :
+            
+            if num < self.target :
+                continue
+                
+            else :
+                return idx
 
-
-
     
     
-    
+sample = Solution()
