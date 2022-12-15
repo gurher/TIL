7c1f1d0734440e95967811a6f96f8ed28658920d
Hyug Her <gurher@ucdavis.edu>
2022-12-14 18:48:05 -0800

    update

--- Python/Leetcode/Data_Structure/DSI_Day2_Array.py
+++ Python/Leetcode/Data_Structure/DSI_Day2_Array.py
@@ -10,6 +10,15 @@ class Solution(object):
             else:
                 goal[nums[i]]=i
             print(goal)
+        # import collections 
+        # dic = collections.defaultdict(list)
+        
+        # for i in range(len(nums)):
+        #     temp = target - nums[i]
+        #     if temp in dic:
+        #         return [i, dic[temp][0]]
+        #     dic[nums[i]].append(i)   
+            
 
     def merge(self, nums1, m, nums2, n):
         
@@ -30,9 +39,4 @@ class Solution(object):
         
         return nums1
         
-sample = Solution()
-
-
-nums = [2,3,4,2,1,2,3,4,5,6,7]
-target = 3
-print(sample.twoSum(nums, target))
+test = Solution()
